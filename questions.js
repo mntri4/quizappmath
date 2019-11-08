fc0558ed79ff5d9f13ec7951c07518bddd1db279
mntri4 <31458021+mntri4@users.noreply.github.com>
2019-11-08 10:52:20 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,65 @@
+const QUESTIONS = [
+    {
+        question: 'How many inches are in five feet?',
+        answers: [
+            { text: 36, correct: false },
+            { text: 48, correct: false },
+            { text: 60, correct: true },
+            { text: 72, correct: false }
+        ]
+    },
+    {
+        question: 'Standard door entrance, is how many feet?',
+        answers: [
+            { text: 'Four feet', correct: false },
+            { text: 'Four plus feet', correct: false },
+            { text: 'Three feet', correct: true },
+            { text: 'Five feet', correct: false }
+        ]
+    },
+    {
+        question: 'How many yards are on a football field?',
+        answers: [
+            { text: 140, correct: false },
+            { text: 110, correct: false },
+            { text: 120, correct: false },
+            { text: 100, correct: true }
+        ]
+    },
+    {
+        question: 'What is 10 raised to the power of 5?',
+        answers: [
+            { text: 100000, correct: true },
+            { text: 10000, correct: false },
+            { text: 100001,correct: false },
+            { text: 1000, correct: false }
+        ]
+    },
+    {
+        question: 'What is 4 to the power of 4?',
+        answers: [
+            { text: 258, correct: false },
+            { text: 256, correct: true },
+            { text: 64, correct: false },
+            { text: 254, correct: false }
+        ]
+    },
+    {
+        question: 'What is 15 X 3?',
+        answers: [
+            { text: 45, correct: false },
+            { text: 46, correct: false },
+            { text: 52, correct: false },
+            { text: 60, correct: true }
+        ]
+    },
+    {
+        question: 'What is 6 X 7?',
+        answers: [
+            { text: '57', correct: false },
+            { text: '42', correct: true },
+            { text: '21', correct: false },
+            { text: '43', correct: false }
+        ]
+    },
+]
